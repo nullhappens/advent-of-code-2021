921b2514e9125f37f9436e1de9a39216782ffb4e
Diego Pineda <nullhappens@users.noreply.github.com>
2021-12-01 21:05:42 -0500

    better error handling

--- src/main/scala/com/nullhappens/aoc/Day1.scala
+++ src/main/scala/com/nullhappens/aoc/Day1.scala
@@ -9,15 +9,16 @@ object Day1 extends IOApp.Simple {
     for {
       lines <- Utilities
         .loadSolutionFile[IO]("/day1.txt")
+        .filter(_.length() > 0)
+        .evalMap(parseInt)
         .compile
         .toList
-        .map(transform)
       _ <- IO.println(s"solution for part 1: ${part1(lines)}")
       _ <- IO.println(s"solution for part 2: ${part2(lines)}")
     } yield ()
 
-  def transform(lst: List[String]): List[Int] =
-    lst.map(s => Either.catchNonFatal(Integer.parseInt(s)).toOption).flatten
+  def parseInt(s: String): IO[Int] =
+    IO.fromEither(Either.catchNonFatal(Integer.parseInt(s)))
 
   def part1(ls: List[Int]): Int =
     ls
